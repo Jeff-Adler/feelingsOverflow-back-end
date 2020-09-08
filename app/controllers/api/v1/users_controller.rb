074b320074b0b2d6056b7a04a98eb971ee86c164
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-08 13:34:31 -0400

    added complete user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,6 @@ class Api::V1::UsersController < ApplicationController
     private
     
       def user_params
-        # params.require(:user).permit(:username, :password, :age, :gender, :location)
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :age, :gender, :location)
       end
 end
