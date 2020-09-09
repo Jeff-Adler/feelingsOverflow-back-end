1923cd9a155b86fa5cfae0659da06b1d001c8190
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-09 19:30:39 -0400

    added error messages for signup validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::UsersController < ApplicationController
         @token = encode_token(user_id: @user.id)
         render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
-        render json: { error: 'failed to create user' }, status: :not_acceptable
+        render json: @user.errors.full_messages, status: :not_acceptable
         end
     end
 
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
     if @user.valid?
         render json: @user.to_json
     else
-        render json: { error: 'failed to edit user' }, status: :not_acceptable
+        render json: { error: 'Failed to edit user' }, status: :not_acceptable
     end
   end
 
