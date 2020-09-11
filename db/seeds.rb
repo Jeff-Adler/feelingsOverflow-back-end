efa13199a5cc849994fd6c68c9303973f976010d
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-11 12:51:34 -0400

    4 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,10 @@ Vote.destroy_all
 user1 = User.create!(username: "Geoffrey", password: Faker::Alphanumeric.alpha(number: 10), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: "Male", location: Faker::Address.city)
 user2 = User.create!(username: "Jeffery", password: Faker::Alphanumeric.alpha(number: 10), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: "Male", location: Faker::Address.city)
 user3 = User.create!(username: "JeffAlder", password: Faker::Alphanumeric.alpha(number: 10), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: "Male", location: Faker::Address.city)
+user4 = User.create!(username: "Tsunade", password: Faker::Alphanumeric.alpha(number: 10), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: "Female", location: "Hidden Leaf Village")
+user5 = User.create!(username: "Orochimaru", password: Faker::Alphanumeric.alpha(number: 10), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: "Male", location: "Hidden Leaf Village")
+user5 = User.create!(username: "Jiraiya", password: Faker::Alphanumeric.alpha(number: 10), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: "Male", location: "Hidden Leaf Village")
+
 
 post1 = Post.create!(mood_rating: "Pretty good", mood_length: "A few weeks", mood_location: "The DOM", 
 mood_trigger: "No", mood_trigger_detail: "", 
@@ -61,6 +65,26 @@ poster_id: user1.id, poster_name: user1.username)
 post2.comments.create!(comment: "Ha, even though I graduated high school 15 years ago, my fondest memories are still from marching band." , commenter_id: user2.id)
 post2.comments.create!(comment: "I feel you, dude. That's the only reason I did sports in high school, the camaraderie.", commenter_id: user3.id)
 
+post3 = Post.create!(mood_rating: "A little down", mood_length: "1-6 months", mood_location: "In my room",
+mood_trigger: "No", mood_trigger_detail: "", 
+mood_description: "My parents don't understand my love of slugs.", 
+mood_purpose: "Get Support", mood_title: "Any slug lovers out there?", 
+mood_category: "Family", mood_category_detail: "", 
+poster_id: user4.id, poster_name: user4.username)
+
+post3.comments.create!(comment: "Slugs are nothing compared to snakes!" , commenter_id: user4.id)
+post3.comments.create!(comment: "But frogs are what really reign supreme.", commenter_id: user5.id)
+
+post4 = Post.create!(mood_rating: "Could be better", mood_length: "Just today", mood_location: "In quarantine",
+mood_trigger: "No", mood_trigger_detail: "", 
+mood_description: "I'm going to miss all my best coding buds!", 
+mood_purpose: "Get Support", mood_title: "Bye Bye 062220!", 
+mood_category: "School", mood_category_detail: "", 
+poster_id: user2.id, poster_name: user2.username)
+
+post3.comments.create!(comment: "062220 is best cohort ever!" , commenter_id: user3.id)
+post3.comments.create!(comment: "I never really liked that Jeff guy much.", commenter_id: user2.id)
+
 Comment.all.each do |comment|
     comment.votes.create!(voter_id: User.all.first.id, upvote: true)
     comment.votes.create!(voter_id: User.all.last.id, upvote: false)
@@ -68,9 +92,3 @@ Comment.all.each do |comment|
     comment.votes.create!(voter_id: User.all.last.id, upvote: false)
 end
 
-# post3 = Post.create!(mood_rating: "A little down", mood_length: "1-6 months", mood_location: "In my room",
-# mood_trigger: "No", mood_trigger_detail: "", 
-# mood_description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4), 
-# mood_purpose: Faker::Lorem.sentence(word_count: 3), mood_title: Faker::Lorem.sentence(word_count: 1), 
-# mood_category: Faker::SlackEmoji.activity, mood_category_detail: Faker::Lorem.sentence(word_count: 3), 
-# poster_id: user.id, poster_name: user.username)
